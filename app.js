601f919a30d63b7297e630dde82123ee50f7a61f
outlander <k2awsprod@gmail.com>
2025-01-05 13:59:28 +1100

    added deployment config

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get('/dummy', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`API is running on http://localhost:${port}`);
 });
+
